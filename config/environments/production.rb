9d4b71134cf0c7ac74c7d8b69dc18b306f480573
Maile Thiesen <mailethiesen@gmail.com>
2016-09-13 22:53:29 +0000

    fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.defaul_url_options = { host: 'grammable-maile-thiesen.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'grammable-maile-thiesen.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
